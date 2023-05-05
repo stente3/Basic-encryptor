1d25f3a522904957033f8aed08317515f9baa671
Stente3 <stentem0103@gmail.com>
2023-05-05 10:32:57 -0500

    Feat(encoding and decoding): Add message encoding and decoding functions and clipboard copy button
    This commit adds two new functions that allow the user to encode and decode messages by clicking on the corresponding buttons.
    Additionally, a new button was added to copy the encoded or decoded text to the clipboard.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
 // Obtener el elemento del textarea
 const textArea = document.querySelector('#main__text-area');
 
+// Obtener elementos del footer
+const footerHeader = document.querySelector('.footer__header');
+const footerParagraph = document.querySelector('.footer__paragraph');
+const encryptionDisplay = document.querySelector('.code');
+const copyButton = document.querySelector('.copy');
+
+// Obtener botones de encriptar y desencriptar
+const encryptionButton = document.querySelector('.main__button--encrypt');
+const decryptionButton = document.querySelector('.main__button--decrypt');
+
 /**
  * Función que convierte el texto del elemento textArea a minúsculas y elimina caracteres especiales.
  * Actualiza el valor del elemento textArea con el nuevo texto limpio.
@@ -22,8 +32,71 @@ function cleanText() {
   // Actualizar el valor del elemento textArea con el nuevo texto limpio
   textArea.value = lowercaseText;
 }
+/**
+ * Función que codifica el texto pasado como argumento, reemplazando las vocales por valores codificados.
+ * Si el texto no contiene ninguna vocal, devuelve el mismo texto sin cambios.
+ */
+function encodeCode(text) {
+  // Encode
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const replacements = ['ai', 'enter', 'imes', 'ober', 'ufat'];
+
+  // Verificar si hay alguna vocal en el texto
+  if (vowels.some(vowel => text.includes(vowel))) {
+    // Reemplazar las vocales en el texto
+    const modifiedText = text
+      .split('')
+      .map(char => {
+        const vowelIndex = vowels.indexOf(char);
+        return vowelIndex > -1 ? replacements[vowelIndex] : char;
+      })
+      .join('');
+    return modifiedText;
+  }
+  return text;
+}
+/**
+ * Función que decodifica el texto codificado pasado como argumento, reemplazando los valores codificados por las vocales originales.
+ * Si el texto no contiene ninguno de los valores codificados, devuelve el mismo texto sin cambios.
+ */
+function decodeCode(encodedText) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const replacements = ['ai', 'enter', 'imes', 'ober', 'ufat'];
+  let decodedText = encodedText;
+
+  // Reemplazar los reemplazos en el texto
+  replacements.forEach((replacement, index) => {
+    decodedText = decodedText.split(replacement).join(vowels[index]);
+  });
+
+  return decodedText;
+}
+
+function encryptionConfigs() {
+  if (textArea.value.length > 0) {
+    footerHeader.classList.add('hide');
+    footerParagraph.classList.add('hide');
+    encryptionDisplay.classList.remove('hide');
+    copyButton.classList.remove('hide');
+  } else {
+    footerHeader.classList.remove('hide');
+    footerParagraph.classList.remove('hide');
+    encryptionDisplay.textContent = '';
+  }
+}
 
 // Escuchar el evento de entrada del usuario para llamar a la función cleanText()
-textArea.addEventListener('input', () => {
-  cleanText();
+textArea.addEventListener('input', cleanText);
+
+encryptionButton.addEventListener('click', () => {
+  encryptionDisplay.textContent = encodeCode(textArea.value);
+  encryptionConfigs();
+});
+decryptionButton.addEventListener('click', () => {
+  encryptionDisplay.textContent = decodeCode(textArea.value);
+  encryptionConfigs();
+});
+
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(encryptionDisplay.textContent);
 });
